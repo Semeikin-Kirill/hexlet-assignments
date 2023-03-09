05d0af4a542f4db49067034d05cb0e71e36891f8
@hexlet/cli <support@hexlet.io>
2023-03-09 08:37:32 +0300

    submit ruby-basics-ru/metaprogramming

--- ruby-basics-ru/metaprogramming/lib/user.rb
+++ ruby-basics-ru/metaprogramming/lib/user.rb
@@ -1,11 +1,3 @@
 # frozen_string_literal: true
 
-require_relative './model'
 
-class User
-  include Model
-
-  attribute :name, type: :string, default: 'Andrey'
-  attribute :birthday, type: :datetime
-  attribute :active, type: :boolean, default: false
-end
--- ruby-basics-ru/metaprogramming/test/post_test.rb
+++ ruby-basics-ru/metaprogramming/test/post_test.rb
@@ -13,30 +13,20 @@ class PostTest < Minitest::Test
       published: true
     }
 
-    @expected_attributes = @attributes.merge(created_at: DateTime.parse(@attributes[:created_at]))
   end
 
   def test_model_contains_attributes_on_initialize
-    post = Post.new(@attributes)
 
     assert true
   end
 
   def test_model_contains_attributes_with_accessors
-    post = Post.new
-
-    post.id = @attributes[:id]
-    post.title = @attributes[:title]
-    post.body = @attributes[:body]
-    post.created_at = @attributes[:created_at]
-    post.published = @attributes[:published]
 
     assert true
   end
 
   def test_each_model_has_its_own_attributes
-    post1 = Post.new title: 'test1'
-    post2 = Post.new title: 'test2'
+
 
     assert true
   end
